66376695d24a1f36f2dc0e83afc044fbf9640397
Mace <maceplus@gmail.com>
2018-08-14 23:45:53 +0100

    index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './portfolio.css';
+import  './icon.png';
+
+/*
++   // Add the image to our existing div.
++   var myIcon = new Image();
++   myIcon.src = Icon;
++
++   element.appendChild(myIcon);
+*/
