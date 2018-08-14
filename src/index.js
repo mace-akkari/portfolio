e403fbc0f76a5bb740fe29982176267cdb7ee539
Mace <maceplus@gmail.com>
2018-08-14 23:47:03 +0100

    change var to let

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import  './icon.png';
 
 /*
 +   // Add the image to our existing div.
-+   var myIcon = new Image();
++   let myIcon = new Image();
 +   myIcon.src = Icon;
 +
 +   element.appendChild(myIcon);
