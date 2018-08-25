3fa19591796fe06b15491fbaece5c3abcfc194fa
Mace <maceplus@gmail.com>
2018-08-26 01:12:38 +0300

    dleted comment

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
 import './portfolio.css';
-import  './icon.png';
-
-/*
-+   // Add the image to our existing div.
-+   let myIcon = new Image();
-+   myIcon.src = Icon;
-+
-+   element.appendChild(myIcon);
-*/
+import  './icon.svg';
