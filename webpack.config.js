9380422202a39171ca1e38297f27e7aeef80f224
Mace <maceplus@gmail.com>
2018-09-13 00:05:44 +0100

    add copy-webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -10,8 +11,11 @@ module.exports = {
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
             title: 'Portfolio',
-            template: './src/index.html'
-        })
+            template: './src/index.html'}),
+        new CopyWebpackPlugin([
+            { from: './src/images/*', to: 'images', flatten: true}
+            //{ from: './src/portfolio.css', to: 'styles'}
+          ]),        
     ],
     output: {
         filename: 'portfolio.bundle.js',
